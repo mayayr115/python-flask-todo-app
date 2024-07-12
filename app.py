bf35714078cc1430de52b141cd649c697d9e43b5
mayayr115 <mayayr115@gmail.com>
2024-07-11 20:02:55 -0400

    Implemented CRUD functions and Added Checkbox

--- app.py
+++ app.py
@@ -1,9 +1,34 @@
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__, template_folder='templates')
-todos = [{'todo': 'Sample Todo', 'done': False}]
+todos = [{'task': 'Sample Todo', 'done': False}]
+
 @app.route("/")
 def index():
     return render_template('index.html', todos=todos)
 
-# if __name__ == '__main__':
-#     app.run(debug=True)
+@app.route('/add', methods=['POST'])
+def add() :
+    todo = request.form['todo']
+    todos.append({'task': todo, 'done': False})
+    return redirect(url_for('index'))
+
+@app.route('/edit/<int:index>', methods=['GET', 'POST'])
+def edit(index):
+    todo = todos[index]
+    if request.method == 'POST':
+        todo['task'] = request.form['todo']
+        return redirect(url_for('index'))
+    else:
+        return render_template('edit.html', todo=todo, index=index)
+
+def check(index):
+    todos[index]['done'] = not todos[index]['done']
+    return redirect(url_for('index'))
+
+@app.route('/delete/<int:index>')
+def delete():
+    del todos[index]
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
